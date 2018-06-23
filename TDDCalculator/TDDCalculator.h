9b9cc1da19cc6eb82cbcf029c373e903965ff1cc
Javier Azuara <me@javierazuara.com>
2018-06-23 07:58:49 -0400

    Initial Commit

---
+++ TDDCalculator/TDDCalculator.h
@@ -0,0 +1,19 @@
+//
+//  TDDCalculator.h
+//  TDDCalculator
+//
+//  Created by Javier Azuara on 2018/06/23.
+//  Copyright © 2018年 Javier Azuara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDDCalculator.
+FOUNDATION_EXPORT double TDDCalculatorVersionNumber;
+
+//! Project version string for TDDCalculator.
+FOUNDATION_EXPORT const unsigned char TDDCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDCalculator/PublicHeader.h>
+
+
